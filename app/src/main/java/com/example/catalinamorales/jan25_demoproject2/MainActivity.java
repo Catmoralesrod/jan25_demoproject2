1dcdfecd8fe20d926e806a23a4309ca3df120e91
Catmoralesrod <cdcmr.2016@gmail.com>
2017-01-25 14:19:35 -0500

    Add button..
    In the main activity

--- app/src/main/java/com/example/catalinamorales/jan25_demoproject2/MainActivity.java
+++ app/src/main/java/com/example/catalinamorales/jan25_demoproject2/MainActivity.java
@@ -2,12 +2,23 @@ package com.example.catalinamorales.jan25_demoproject2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    int count= 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void monClick(View view) {
+
+        ((Button)view).setText("Clicked" + (++count));
+
+
     }
 }
